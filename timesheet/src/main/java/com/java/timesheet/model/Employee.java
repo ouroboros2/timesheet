d9bd5ddc4349b1070ecc24cbe4cd3d1e33ed7097
63917 <63917@LAPTOP-QVIK9BKB>
2021-03-29 08:08:58 +0800

    Employee Class Changes

--- timesheet/src/main/java/com/java/timesheet/model/Employee.java
+++ timesheet/src/main/java/com/java/timesheet/model/Employee.java
@@ -1,5 +1,73 @@
 package com.java.timesheet.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity(name = "task")
+@Table(name = "task")
 public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "task_seq")
+	@SequenceGenerator(name = "task_seq", sequenceName = "task_seq", initialValue = 5550, allocationSize = 1)
+	@Column(name = "employeeid")
+	private int employeeid;
+
+	@Column(name = "firstName")
+	private String firstName;
+
+	@Column(name = "lastName")
+	private String lastName;
+
+	@Column(name = "userName")
+	private String userName;
+
+	@Column(name = "password")
+	private String password;
+
+	public int getEmployeeid() {
+		return employeeid;
+	}
+
+	public void setEmployeeid(int employeeid) {
+		this.employeeid = employeeid;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
