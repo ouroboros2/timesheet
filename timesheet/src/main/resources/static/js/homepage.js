79c39f9d811450039323343bd3e2e0ad77f2966d
63917 <63917@LAPTOP-QVIK9BKB>
2021-03-31 11:29:23 +0800

    JS changes

--- timesheet/src/main/resources/static/js/homepage.js
+++ timesheet/src/main/resources/static/js/homepage.js
@@ -1,5 +1,49 @@
-$(document).ready(function(){
-        alert("potek");
-});
+function addToTimesheet() {
+		getCheckedTasks();	
+}
 
-alert("Helloq112312");
+function getCheckedTasks() {
+	
+	var taskList = [];
+	var taskListObjs = [];
+	$('input[name=tasks]:checked').map(function() {
+		taskList.push($(this).val());
+	
+	for(var i= 0; i<taskList.length; i++) {
+		
+		var taskDetails = taskList[i].split("-");
+		var taskobj = {
+			projectCode:taskDetails[0],
+			projectId:taskDetails[1],
+			projectDesc:taskDetails[2],
+			projectCategory:taskDetails[3]
+			
+		}
+		taskListObjs.push(taskobj);
+	}
+	
+	for(var i= 0; i<taskListObjs.length; i++) {
+		
+		const cprojectId = document.createElement('td');
+		const cprojectDesc = document.createElement('td');
+		const cprojectCategory = document.createElement('td');
+		
+		
+		cprojectId.textContent = taskListObjs[i].projectCode + taskListObjs[i].projectId;
+		cprojectDesc.textContent = taskListObjs[i].projectDesc;
+		cprojectCategory.textContent = taskListObjs[i].projectCategory;
+		
+		$("#taskTable").append(
+			$('<tr>', {id: "data" + taskListObjs[i].projectId}).append(cprojectId, cprojectDesc, cprojectCategory)
+		);
+		
+		var count = 0
+		while(count < 7) {
+			$("#" + "data" + taskListObjs[i].projectId).append("<td><input size='2'></td>");	
+			count++;
+		}
+	}
+	taskList = [];
+});
+	
+}
