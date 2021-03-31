3348c4a0b0c09cea814ab2a90b9cf45951f74fa7
RogelAlgero.Hernand <RogelAlgero.Hernand@PDOCL9CDXWT2.int.asurion.com>
2021-03-31 19:42:49 +0800

    comment

--- timesheet/src/main/resources/static/js/homepage.js
+++ timesheet/src/main/resources/static/js/homepage.js
@@ -28,7 +28,7 @@ function getCheckedTasks() {
 		const cprojectDesc = document.createElement('td');
 		const cprojectCategory = document.createElement('td');
 		
-		
+		//comment here
 		cprojectId.textContent = taskListObjs[i].projectCode + taskListObjs[i].projectId;
 		cprojectDesc.textContent = taskListObjs[i].projectDesc;
 		cprojectCategory.textContent = taskListObjs[i].projectCategory;
