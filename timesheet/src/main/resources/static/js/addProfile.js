1629c9c792dd29187aad41ff68f0653fa75f31d7
IB <IB@IB>
2021-04-03 20:22:38 +0800

    Added css and js to addProfile

---
+++ timesheet/src/main/resources/static/js/addProfile.js
@@ -0,0 +1,38 @@
+$('#field_type').change(function() {
+	let role = $(this).val();
+	
+	if(role === 'admin' || role === 'manager') {
+		$("#field_manager").hide();
+	} else if(role === 'employee') {
+		$("#field_manager").show();
+	}
+});
+
+$('#field_confpass').focusout(function() {
+	let pass = $('#field_pass').val();
+	let pass2 = $('#field_confpass').val();
+	if (pass !== pass2) {
+		$.alert({
+			title: 'Alert!',
+			content: 'Passwords doesn\'t match!',
+		});
+
+		$(':input[type="submit"]').prop('disabled', true);
+	} else {
+		$(':input[type="submit"]').prop('disabled', false);
+	}
+});
+
+$('a.btn')
+	.confirm(
+		{
+			title: '',
+			content: 'Are you sure you want to cancel? Your inputs will not be saved.',
+
+			buttons: {
+				yes: function() {
+					location.href = this.$target.attr('href');
+				},
+				no: {}
+			}
+		});
